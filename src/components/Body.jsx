fa751e6172503ba8771ae8d61178d3387ea426f7
Chinmay <zchinmayz@gmail.com>
2025-08-29 00:34:46 +0530

    added tailwindCSS

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,6 +5,7 @@ import resList from "../utils/dummyData";
 import { API_URL } from "../utils/constants";
 import Shimmer from "./Shimmer";
 import { Link, Links } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
     
@@ -32,45 +33,55 @@ const Body = () => {
             SetRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
             SetfiltreredlistOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
     }
+    
+    const onlineStatus = useOnlineStatus();
+    if(onlineStatus === false) return(
+        <h1>
+            Aww no internet 😭
+        </h1>
+    )
 
     return(
         listOfRestaurant.length === 0 ? (
             <Shimmer/>
         ) : (
             <div className="body">
-                <div className="toprated">
-                    <button className="toprated-btn" 
-                    onClick={() => {
-                        const filteredList = listOfRestaurant.filter((res) => (
-                            res.info.avgRating > 4.5
-                        ))
-                        SetRestaurant(filteredList);
-                    }}
-                    >Top Rated Restaurants</button>
-                </div>
-                <div className="filter-search">
-                    <input 
-                        className="filter-in"
-                        placeholder="  search..." 
-                        type="text" 
-                        value={filterSearch} 
-                        onChange={(e)=>{
-                            setfilterSearch(e.target.value);
+                    <div className="filter-controls flex items-center justify-between m-4">
+                    <button className="toprated-bt bg-blue-500 text-white px-4 py-2 rounded mr-4" 
+                        onClick={() => {
+                            const filteredList = listOfRestaurant.filter((res) => (
+                                res.info.avgRating > 4.5
+                            ))
+                            SetfiltreredlistOfRestaurant(filteredList);
                         }}
-                    />
-                    <button className="filter-btn" type="button" 
-                         onClick={() => {
-                            const filteredRestaurants = listOfRestaurant.filter((res) => 
-                            res.info.name.toLowerCase().includes(filterSearch.toLowerCase())
-                            );
+                    >
+                        Top Rated Restaurants
+                    </button>
 
-                        SetfiltreredlistOfRestaurant(filteredRestaurants);
-                        // console.log(filteredRestaurants);
-
-                        }}
-                    >Enter</button>
+                    <div className="filter-search flex items-center">
+                        <input 
+                            className="border border-solid pl-2 py-1 rounded mr-2"
+                            placeholder="Search..." 
+                            type="text" 
+                            value={filterSearch} 
+                            onChange={(e)=>{
+                                setfilterSearch(e.target.value);
+                            }}
+                        />
+                        <button className="bg-green-500 px-4 py-2 text-white rounded"
+                            type="button" 
+                            onClick={() => {
+                                const filteredRestaurants = listOfRestaurant.filter((res) => 
+                                    res.info.name.toLowerCase().includes(filterSearch.toLowerCase())
+                                );
+                                SetfiltreredlistOfRestaurant(filteredRestaurants);
+                            }}
+                        >
+                            Enter
+                        </button>
+                    </div>
                 </div>
-                <div className="res-container" style={{backgroundColor:"#f0f0f0"}}>
+                <div className="flex flex-wrap">
                     {filtreredlistOfRestaurant.map((resto) => (
                         <Link
                             key={resto.info.id}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
 
     // const btnName = "Login";
     const[btnName, setbtnName] = useState("Login");
+    const onlineStatus = useOnlineStatus();
     return(
-    <div className="header">
-        <img className="logocontainer"
+
+    <div className="flex justify-between bg-pink-200 shadow-lg m-2">
+        <img className="w-39"
         src="src/utils/lickin.png"/>
 
-        <div className="nav-items">
-            <ul>
-                <li> <Link to="/" >Home</Link> </li>
-                <li><Link to="/about" >About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-                <li>Cart</li>
+        <div className="flex items-center">
+            <ul className="flex p-4 m-4 mt-9 ">
+                <li className="pr-3.5">
+                    {onlineStatus ? "✅": "❌"}
+                </li>
+                <li className="pr-4"> <Link to="/" >Home</Link> </li>
+                <li className="pr-4">Cart</li>
+                <li className="pr-4"><Link to="/about" >About</Link></li>
+                <li className="pr-4"><Link to="/contact">Contact</Link></li>
+                <li className="pr-4">Cart</li>
                 <button className="login" onClick={() => {
                     btnName === "Logout" 
                     ? setbtnName("Login") 
